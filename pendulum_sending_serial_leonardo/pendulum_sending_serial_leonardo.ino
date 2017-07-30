f6991b188885ad5c3158ff59e3ee25e5ef94ce75
hydronics <hydronics@gmail.com>
2017-07-29 19:46:44 -0700

    Signed-off-by: hydronics <hydronics@gmail.com>

--- pendulum_sending_serial_leonardo/pendulum_sending_serial_leonardo.ino
+++ pendulum_sending_serial_leonardo/pendulum_sending_serial_leonardo.ino
@@ -7,20 +7,19 @@
 //if the averageValue is graeter than ~2500 it sends serial to the ESP at 115200
 /// data format is '[' start, COCOON#, 0-255 tap strength, ']' ending character
 
-
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_NeoPixel.h>
+#include "CocoonLEDs.h"
 
-#define COCCOON 9
-#define PIN 6
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(15, PIN, NEO_GRB + NEO_KHZ800);
+#define COCOON               (9)
+#define TAP_THRESHOLD        (1000)
+#define BLACKOUT_THRESHOLD   (2000)
+#define DEV_BLINK            (false)
 
 // I2C
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
-
 
 long currentTime = 0;
 int lastAverageAcc = 0;
@@ -29,79 +28,80 @@ const int sizeRolling = 20;
 int rollingAcc[sizeRolling];
 int incrementRolling = 2;
 
-int yNeutral = 0; 
-int zNeutral = 0;   
+int yNeutral = 0;
+int zNeutral = 0;
 
 long lastZeroHighTime = 0;
 
 int averageHigh = 0;
 int averageHighLast = 0;
 
+uint8_t blink = 0;
+const uint8_t BUILTIN_LED_PIN = 17;
+
 int serialChar = 0;
 int serialInput[26];
 boolean stringComplete = false;
 long lastTimeSent = 0;
 
-
-
 void setup(void) {
+  pinMode(BUILTIN_LED_PIN, OUTPUT);
+
+  // Generate numbers with randomness
+  randomSeed(analogRead(0) << 10 | analogRead(1));
+
   Serial.begin(115200);
   Serial1.begin(115200);
+
+  cocoon_leds_init();
+
   Serial.println("LIS3DH test!");
   if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
     Serial.println("Couldnt start");
     while (1);
   }
   Serial.println("LIS3DH found!");
-  
-  lis.setRange(LIS3DH_RANGE_2_G);   // 2, 4, 8 or 16 G!
+
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
 
   //1, 10, 25, 50, 100, 200, 400_HZ, LOWPOWER_1K6HZ, _5KHZ
   lis.setDataRate(LIS3DH_DATARATE_400_HZ);
-  
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());
   Serial.println("G");
 
-  Serial.print("Data rate = "); Serial.print(lis.getDataRate());  
+  Serial.print("Data rate = "); Serial.print(lis.getDataRate());
   Serial.println("Hz");
-
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
 
   //capture the near neutral position of the 200G accerometer..at 12bit or 50% of 0-4095=2048
     long y2 = 0;
     long z2 = 0;
   for(int i = 0; i<20; i++){
     lis.read();      // get X Y and Z data at once
-    int y1 = lis.y;  
+    int y1 = lis.y;
     y1 = abs(y1);
     y2 = y1 + y2;
     delay(5);
-    int z1 = lis.z; 
+    int z1 = lis.z;
     z1 = abs(z1);
     z2 = z1 + z2;
     delay(5);
   }
   yNeutral = y2/20;
-  Serial.print("yNeutral: ");
-  Serial.println(yNeutral);
-  Serial.print("zNeutral: ");
+  //Serial.print("yNeutral: ");
+  //Serial.println(yNeutral);
+  //Serial.print("zNeutral: ");
   zNeutral = z2/20;
-  Serial.println(zNeutral); 
+  //Serial.println(zNeutral);
 }
 
 
 void loop() {
+  // Blink to prove that we're alive
+  if (!DEV_BLINK) blink = 1;
+  digitalWrite(BUILTIN_LED_PIN, (blink == 0) ? LOW : HIGH);
+  blink = (blink + 1) & 0x3f;
 
-  
-//  lastAverageAcc = 3000;
-//  
-//  if(currentTime - lastTimeSent > 5000){
-//    sendTapData();
-//    lastTimeSent = currentTime;
-//  }
-  
-  
   serialEvent();  //look for Serial data coming in...
 
   currentTime = millis();
@@ -113,7 +113,6 @@ void loop() {
     //Serial1.write(inByte);
     Serial1.write(inByte);
   }
-  
 
   lis.read();      // get X Y and Z data at once
   int zg = lis.z;
@@ -138,22 +137,38 @@ void loop() {
       acc = yg;
     }
   }
-  
+
   delay(2); //reading at 400hz
-  rollingAcc[incrementRolling] = acc;   
+  rollingAcc[incrementRolling] = acc;
   incrementRolling++;
   if(incrementRolling == sizeRolling){
     incrementRolling = 0;
-    }
-  findAverage();
-  if(lastAverageAcc > 4000){ //TAP detected send it to ESP
-    sendTapData();
-  int randomColor = random(0,255);
-  colorWipe(Wheel(randomColor), 0); //
-  memset(rollingAcc, 0, sizeof(rollingAcc)); //clear array so it doesn't take forever to normalize
-  delay(400); //
-  return;
   }
+
+  findAverage();
+
+  if(lastAverageAcc > TAP_THRESHOLD){ //TAP detected send it to ESP
+
+    // Hard hit? Do a blackout
+    if (lastAverageAcc > BLACKOUT_THRESHOLD) {
+      cocoon_do_blackout(0);
+
+      memset(rollingAcc, 0, sizeof(rollingAcc)); //clear array so it doesn't take forever to normalize
+
+      return;
+    }
+
+    cocoon_leds_start_new_color();
+    uint32_t color = cocoon_get_current_color();
+    cocoon_do_color_tween(color, 0);
+
+    sendTapData(color);
+    
+    memset(rollingAcc, 0, sizeof(rollingAcc)); //clear array so it doesn't take forever to normalize
+
+    return;
+  }
+
   if(lastAverageAcc > averageHigh){ //keeps track of high.. shows how much the pendulum is swinging
     averageHigh = lastAverageAcc;
   }
@@ -163,6 +178,8 @@ void loop() {
     averageHigh = 0;
     //Serial.println(averageHighLast);
   }
+
+  cocoon_leds_update(lastAverageAcc);
 }
 
 void findAverage(){
@@ -171,27 +188,30 @@ void findAverage(){
     averageAcc = rollingAcc[i] + averageAcc;
   }
   lastAverageAcc = averageAcc/sizeRolling;
-  Serial.println(lastAverageAcc);
+  //Serial.println(lastAverageAcc);
 }
 
 // sends data to ESP at 115200
 // sending Coccoon#, Red,Green,Blue, Intensity
-void sendTapData(){
+void sendTapData(uint32_t currentColor){
   Serial1.write('['); //start value
   Serial.println('['); //start value
   
-  Serial1.write(COCCOON);
-  Serial.println(COCCOON);
+  Serial1.write(COCOON);
+  Serial.println(COCOON);
+
+  int red = ((currentColor & 0xff0000) >> 16) / 255.0f;
+  int green = ((currentColor & 0x00ff00) >>  8) / 255.0f;
+  int blue = ((currentColor & 0x0000ff)      ) / 255.0f;
   
-  byte randomNumber = (random(0,255));
-  Serial1.write(randomNumber); //red
-  Serial.println(randomNumber); //red
-  randomNumber = (random(0,255));
-  Serial1.write(randomNumber); //green
-  Serial.println(randomNumber); //green
-  randomNumber = (random(0,255));
-  Serial1.write(randomNumber); //blue  
-  Serial.println(randomNumber); //blue
+  Serial1.write(red); //red
+  Serial.println(red); //red
+
+  Serial1.write(green); //green
+  Serial.println(green); //green
+
+  Serial1.write(blue); //blue  
+  Serial.println(blue); //blue
   
   char tapValue = map(lastAverageAcc, 2000,6000,0,255); 
   Serial1.write(tapValue); //tap value
@@ -227,30 +247,3 @@ void serialEvent() {
     }
   }
 }
-
-
-
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
